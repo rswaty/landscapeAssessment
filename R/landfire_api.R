57ed19e56ef006b1e160d116dda73f722c3f4760
bcknr <mab677@cornell.edu>
2022-09-27 10:25:43 -0400

    Minor fixes for error reporting

--- R/landfire_api.R
+++ R/landfire_api.R
@@ -1,17 +1,19 @@
-
-#' Call LANDFIRE API from R - Needs work. 
+#' LANDFIRE API from R
+#'
+#' @description
+#' `landfireApi` calls the LANDFIRE REST API directly from R
 #'
 #' @param products Product names as character vector (see: https://lfps.usgs.gov/helpdocs/productstable.html)
 #' @param aoi Area of interest as character or numeric vector ordered xmin, ymin, xmax, ymax
 #' @param projection Single numeric value of the EPSG code for preferred output projection
 #' @param resolution Single numeric value specifying the preferred resolution in meters
-#' @param edit_rule Optional **Not currently functional 
-#' @param edit_mask Optional **Not currently functional
-#' @param path Path to write zip file. Passed to download.file().
+#' @param edit_rule Optional **Not currently functional**
+#' @param edit_mask Optional **Not currently functional**
+#' @param path Path to .zip directory which is passed to `utils::download.file()`. Created if does not exist.
 #' @param max_time Maximum time, in seconds, to wait for job to be completed
-#' @param method Passed to `download.file()` see ?download.file
+#' @param method Passed to `utils::download.file()` see ?download.file
 #'
-#' @return 
+#' @return Returns API call passed from httr::get(). Writes files to specified path.
 #' @export
 #'
 #' @examples
@@ -24,7 +26,7 @@
 #' test <- landfire_api(products, aoi, projection, resolution, path = save_file, max_time = 540)
 #' }
 
-landfire_api <- function(products, aoi, projection, resolution, edit_rule = NULL, edit_mask = NULL, path, max_time = 600, method = "curl") {
+landfireApi <- function(products, aoi, projection, resolution, edit_rule = NULL, edit_mask = NULL, path, max_time = 600, method = "curl") {
   base_url <- httr::parse_url("https://lfps.usgs.gov/arcgis/rest/services/LandfireProductService/GPServer/LandfireProductService/submitJob?")
   base_url$query <- list(Layer_List = paste(products, collapse = ";"),
                          Area_of_Interest = paste(aoi, collapse = " "),
@@ -36,15 +38,15 @@ landfire_api <- function(products, aoi, projection, resolution, edit_rule = NULL
   
   url <- httr::build_url(base_url)
   r <- httr::GET(url)
-  job_id <- str_extract(r$url, ".{33}$") #NOTE: Assumes that job id length is always 33 characters
+  job_id <- stringr::str_extract(r$url, ".{33}$") #NOTE: Assumes that job id length is always 33 characters
   dwl_url <- paste0("https://lfps.usgs.gov/arcgis/rest/directories/arcgisjobs/landfireproductservice_gpserver/", job_id, "/scratch/", job_id, ".zip")
   
   cat("Job Status: Submitted\nVisit url to track status:\n  ", r$url,"\n")
   
-  #API always returns a successful status code (200) even if file is not ready. 
+  #API always returns a successful status code (200) even if file is not ready.
   if(httr::status_code(r) == 200 & grepl("Succeeded at", httr::content(r, "text"), fixed = TRUE)) {
     cat("Job Status: Complete.\nDownloading file\n")
-    download.file(dwl_url, path, method = method)
+    utils::download.file(dwl_url, path, method = method)
     
   } else if(httr::status_code(r) == 200 & !grepl("Succeeded at", httr::content(r, "text"), fixed = TRUE)) {
     cat("Job Status: Executing \n")
@@ -55,7 +57,7 @@ landfire_api <- function(products, aoi, projection, resolution, edit_rule = NULL
       
       if(grepl("Succeeded at", httr::content(r2, "text"), fixed = TRUE)){
         cat("Job Status: Complete.\nDownloading file\n")
-        download.file(dwl_url, path, method = method)
+        utils::download.file(dwl_url, path, method = method)
         break
       }
       
@@ -66,6 +68,10 @@ landfire_api <- function(products, aoi, projection, resolution, edit_rule = NULL
         cat("\n")
         stop("Job status: Incomplete and max_time reached\nIncrease max_time or visit URL to check status and download manually\n   ", r$url)
       }
+    }
+    
+    if(grepl("Failed", httr::content(r2, "text"), fixed = TRUE)){
+      stop("Job status: Failed\nCheck URL for details\n  ", r$url)
     }
     
   } else if(httr::status_code(r) != 200) {
